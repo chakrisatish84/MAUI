1fe6ccfd81342415a5663f0c6044bb8bdb308e39
Sateesh Chava <v-satc@microsoft.com>
2024-01-27 21:29:07 -0800

    Publish MAUI Todo API

---
+++ TodoMauiAPI/Data/AppDBContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoMauiAPI.Models;
+
+namespace TodoMauiAPI.Data
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Todo> ToDos => Set<Todo>();
+    }
+}
+
